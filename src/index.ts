9aa4eda5fe164a922f93d1477e9e912ab8cdf36f
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-04-24 11:55:06 +0900

    change port by enviornment variable

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ const app: express.Express = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3001, () => {
-  console.log('Start on port 3001...')
+const port = process.env.PORT ?? 3001
+
+app.listen(port, () => {
+  console.log(`Start on port ${port}...`)
 })
 
 app.get('/', (req: express.Request, res: express.Response) => {
